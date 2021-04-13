269c4ca630335d480a49cd068180d67b4d35e68f
mauricesvp <maurice@walny.de>
2021-04-13 12:05:17 +0200

    Backend: Add name to log format

--- backend/mousse/log.py
+++ backend/mousse/log.py
@@ -7,7 +7,9 @@ import logging
 
 
 def setup_logger(name: str) -> logging.Logger:
-    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+    )
 
     file_handler = logging.FileHandler(filename="mousse.log")
     stderr_handler = logging.StreamHandler()
