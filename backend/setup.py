7a51e924f29969aae585e8fdcf6740140d261e4e
mauricesvp <maurice@walny.de>
2023-03-17 14:56:48 +0100

    double quotes

--- backend/setup.py
+++ backend/setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mousse',
-    version='0.1.0',
-    description='Module Search Super Easy',
-    author='mauricesvp',
-    url='https://github.com/mauricesvp/mousse',
+    name="mousse",
+    version="0.1.0",
+    description="Module Search Super Easy",
+    author="mauricesvp",
+    url="https://github.com/mauricesvp/mousse",
     package_data={"mousse": ["py.typed"]},
     packages=find_packages(),
     zip_safe=False,
